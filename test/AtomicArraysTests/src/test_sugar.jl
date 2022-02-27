56ec4bb6601134c9d60f4d8310d52d6aee7fc8f1
Takafumi Arakaki <aka.tkf@gmail.com>
2022-02-27 04:40:43 -0500

    Add `@atomicreplace` and `@atomicswap`

--- test/AtomicArraysTests/src/test_sugar.jl
+++ test/AtomicArraysTests/src/test_sugar.jl
@@ -1,6 +1,6 @@
 module TestSugar
 
-using AtomicArrays: @atomic
+using AtomicArrays: @atomic, @atomicreplace, @atomicswap
 using Test
 
 function test_get()
@@ -23,6 +23,24 @@ function test_inc()
     @test A[1] === 124
     @test (@atomic A[end] += 123) === 124
     @test A[end] === 124
+end
+
+function test_swap()
+    A = [1, 1]
+    @test (@atomicswap A[1] = 123) === 1
+    @test A[1] === 123
+    @test (@atomicswap A[end] = 456) === 1
+    @test A[end] === 456
+end
+
+function test_cas()
+    A = [1, 1]
+    @test (@atomicreplace A[1] 1 => 123) == (old = 1, success = true)
+    @test A[1] === 123
+    @test (@atomicreplace A[1] 1 => 456) == (old = 123, success = false)
+    @test A[1] === 123
+    @test (@atomicreplace A[end] 1 => 789) == (old = 1, success = true)
+    @test A[end] === 789
 end
 
 end  # module
