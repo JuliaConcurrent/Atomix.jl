a23d413b1269f2fed5fd8c406ae6e272c3acb0fb
Takafumi Arakaki <aka.tkf@gmail.com>
2022-05-07 11:11:12 -0400

    More documentation on `IndexableRef`

--- src/references.jl
+++ src/references.jl
@@ -1,13 +1,66 @@
 """
-    referenceable(mutable) -> recorder
+    Atomix.IndexableRef{Indexable}
 
-Return a `recorder` such that index and field access yield a ref to the corresponding memory
-location.
+An object `IndexableRef(data, indices)` represents a *reference* to the location
+`data[indices...]`.
+
+A reference object supports `Atomix.pointer`, `Atomix.asstorable`, and `eltype`:
+
+```jldoctest
+julia> using Atomix
+
+julia> a = [111, 222, 333];
+
+julia> ref = Atomix.IndexableRef(a, (1,));
+
+julia> Atomix.pointer(ref) === pointer(a, 1)
+true
+
+julia> Atomix.asstorable(ref, 1.0)
+1
+
+julia> eltype(ref)
+$Int
+```
+
+To customize the behavior of atomic updates of an `Indexable`, define the following methods:
+
+* `Atomix.get(ref::Atomix.IndexableRef{Indexable}, order) -> v::eltype(ref)`
+* `Atomix.set!(ref::Atomix.IndexableRef{Indexable}, v, order)`
+* `Atomix.replace!(ref::Atomix.IndexableRef{Indexable}, expected, desired, success_order,
+  failure_order) -> (; old, success)`.
+* `Atomix.modify!(ref::Atomix.IndexableRef{Indexable}, op, x, order) -> (old => new)`
+
+The ordering arguments (`order`, `success_order`, and `failure_order`) are one of:
+
+* `Atomix.monotonic`
+* `Atomix.acquire`
+* `Atomix.release`
+* `Atomix.acquire_release` (`Atomix.acq_rel`)
+* `Atomix.sequentially_consistent` (`Internal.seq_cst`)
+"""
+Atomix.IndexableRef
+
+Atomix.IndexableRef(data, indices) = IndexableRef(data, indices)
+
+struct IndexableRef{Indexable,Indices} <: Atomix.IndexableRef{Indexable}
+    data::Indexable
+    indices::Indices
+end
+
+"""
+    referenceable(mutable) -> r
+
+Return a *referenceable* object `r` whose index and field access yield a *reference* to the
+corresponding memory location instead of the value stored.
 
 TODO: record field access
 
 TODO: record nested field and index access to update nested element (e.g.,
 array-of-large-structs) atomically?
+
+Note: The idea is identical to Referenceables.jl but Atomix.jl has a minimal implementation
+in its internal.
 """
 function referenceable end
 
