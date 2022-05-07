523ae2704a3bbe0fd827bb6b5bc2e8fcaa6af486
Takafumi Arakaki <aka.tkf@gmail.com>
2022-05-07 08:54:29 -0700

    Setup documentation (#29)

--- src/generic.jl
+++ src/generic.jl
@@ -1,5 +1,54 @@
 # TODO: Support Symbol ordering
 
+"""
+    Atomix.get(ref, order) -> x
+    Atomix.get(ref) -> x
+
+Atomically load the value `x` stored in `ref` with ordering `order`.  The default ordering
+`Atomix.sequentially_consistent` is used when not specified.
+"""
+Atomix.get
+
+"""
+    Atomix.set!(ref, x, order)
+    Atomix.set!(ref, x)
+
+Atomically store the value `x` in `ref` with ordering `order`.  The default ordering
+`Atomix.sequentially_consistent` is used when not specified.
+"""
+Atomix.set!
+
+"""
+    Atomix.modify!(ref, op, x, order) -> (old => new)
+    Atomix.modify!(ref, op, x) -> (old => new)
+
+Atomically update `ref` from stored value `old` to `new = op(old, x)` with ordering `order`
+(default: `Atomix.sequentially_consistent`).  Return a pair `old => new`.
+"""
+Atomix.modify!
+
+"""
+    Atomix.swap!(ref, new, order) -> old
+    Atomix.swap!(ref, new) -> old
+
+Swap the `new` value with the `old` stored in `ref` with ordering `order` (default:
+`Atomix.sequentially_consistent`).
+
+Notes for implementers: `Atomix.swap!(ref, new, order)` is defined as `Atomix.modify!(ref,
+Atomix.right, x, order)`.  Thus, only `Atomix.modify!` has to be defined.
+"""
+Atomix.swap!
+
+"""
+    Atomix.replace!(ref, expected, desired, success_order, fail_order) -> (; old, success)
+    Atomix.replace!(ref, expected, desired, order) -> (; old, success)
+    Atomix.replace!(ref, expected, desired) -> (; old, success)
+
+Atomically replace the value stored in `ref` to `desired` if `expected` is stored.  A named
+tuple `(; old::eltype(ref), success::Bool)` is returned.
+"""
+Atomix.replace!
+
 @inline Atomix.get(ref) = Atomix.get(ref, seq_cst)
 @inline Atomix.set!(ref, x) = Atomix.set!(ref, x, seq_cst)
 
