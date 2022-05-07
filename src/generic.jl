47437a227e4b5bafe50c826092132b198f0fd1bf
Takafumi Arakaki <aka.tkf@gmail.com>
2022-05-07 05:18:05 -0400

    Style fix

--- src/generic.jl
+++ src/generic.jl
@@ -3,8 +3,7 @@
 @inline Atomix.get(ref) = Atomix.get(ref, seq_cst)
 @inline Atomix.set!(ref, x) = Atomix.set!(ref, x, seq_cst)
 
-@inline Atomix.modify!(ref, op::OP, x) where {OP} =
-    Atomix.modify!(ref, op, x, seq_cst)
+@inline Atomix.modify!(ref, op::OP, x) where {OP} = Atomix.modify!(ref, op, x, seq_cst)
 
 @inline Atomix.replace!(ref, expected, desired, order::Ordering = seq_cst) =
     Atomix.replace!(ref, expected, desired, order, order)
